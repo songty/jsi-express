2356eb33fc485d3c80321c232dedc717b696942d
Whitney Young <wbyoung@fadingred.com>
2014-06-17 16:00:19 -0700

    Full server.

--- server.js
+++ server.js
@@ -7,17 +7,44 @@ app.use(require('body-parser')());
 app.use(require('method-override')('_method'));
 app.use(express.static(__dirname + '/public'));
 
-
 var people = {};
+var peopleSequence = (function() {
+  var sequence = 1;
+  return function() {
+    var result = sequence;
+    sequence += 1;
+    return result;
+  };
+}());
+
+app.get('/', function(req, res) {
+  res.redirect('/home/');
+});
+
+app.get('/api/people', function(req, res) {
+  res.json({ people: _.values(people) });
+});
 
 app.post('/api/people', function(req, res) {
-  var id = _.size(people) + 1;
+  var id = peopleSequence();
   var person = {
-    id: 1,
+    id: id,
     name: req.param('name')
   };
   people[id] = person;
   res.json({ person: person });
+});
+
+app.put('/api/people/:id', function(req, res) {
+  var person = people[req.params.id];
+  person.name = req.body.name;
+  res.json({ person: person });
+});
+
+app.delete('/api/people/:id', function(req, res) {
+  var deleted = !!people[req.params.id];
+  delete people[req.params.id];
+  res.json({ status: deleted ? 'deleted' : 'ok' });
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
