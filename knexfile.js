3d4b12624088bacc455bb9182986c16621c63358
Tian Song <tian.y.song@gmail.com>
2014-06-19 15:32:01 -0700

    edited knexfile.

--- knexfile.js
+++ knexfile.js
@@ -29,12 +29,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
