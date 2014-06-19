8337737f9386e8e930d6526ee1bb3955e753e806
Tian Song <tian.y.song@gmail.com>
2014-06-19 14:33:27 -0700

    added posgresql to heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var _ = require('lodash');
 var express = require('express');
 var app = express();
 var query = require('./query');
+var bluebird = require('bluebird'), Promise = bluebird;
+var pg = bluebird.promisifyAll(require('pg'));
 
 app.use(require('morgan')('dev'));
 app.use(require('body-parser')());
@@ -60,3 +62,24 @@ app.delete('/api/people/:id', function(req, res) {
 var server = app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
+
+if (require.main === module) {
+  var settings = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    dbURL: process.env.DATABASE_URL ||
+      'postgres://localhost/jsi-express'
+  };
+
+  pg.connectAsync(settings.dbURL).spread(function(client, done) {
+    createApp(settings, bluebird.promisifyAll(client))
+    .listen(settings.port, function() {
+      console.log('Express server started on port %s', settings.port);
+    });
+  })
+  .catch(function(e) {
+    console.log(e);
+    console.error('Could not connect to database: %s', settings.dbURL);
+    process.exit(1);
+  });
+}
